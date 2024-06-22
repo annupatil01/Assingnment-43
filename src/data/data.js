f19a8f7112c05c10e6ef7c2bcf2fb98b38109d34
annupatil01 <anushkapatil16003@gmail.com>
2024-06-23 00:45:12 +0530

    ass.done

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+import img1 from "./chocolate  Cake.jpg"
+import img2 from "./photo cake.jpg"
+import img3 from "./red forest.jpg"
+import img4 from "./whait forest.jpg"
+const siteDetails={
+    title: "🎂 K2's CAKE SHOP 🎂",
+    description: "Everyone has a favorite dessert; for many people, a cake is one of those.",
+    contactDetails:"Ajinkya-Tara road , Chalisgaon , Maharashtra , India",
+    showFeatures: true,
+}
+const features=[
+    {
+        title: "Chocolate Cake ",
+        img:img1,
+    },
+    {
+        title: "Photo-frame Cake",
+        img:img2,
+    },
+    {
+        title: "Red-forest Cake",
+        img:img3,
+    },
+    {
+        title: "White-forest Cake",
+        img:img4,
+    }
+]
+const theme={
+    primaryColor:"pink",
+    secondaryColor:" #008080",
+    text:"black"
+}
+
+export {siteDetails,features,theme}
